f3cffa9867d59551dceee49582f2263b4daa78df
Sumil <sumilprincejain@gmail.com>
2019-03-08 18:30:10 +0530

    hiding navigation button on edges

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import question from './questionJson.json';
-import Question from './components/Question';
+import React, { Component } from "react";
+import "./App.css";
+import question from "./questionJson.json";
+import Question from "./components/Question";
 
 class App extends Component {
-  state = {question,selectedQues:0};
+  state = { question, selectedQues: 0 };
 
   render() {
+    console.log(this.state.selectedQues);
     return (
       <div className="App">
         <Question data={this.state.question} selectedQues={this.state.selectedQues}/>
-        <button onClick={()=>this.setState({selectedQues:this.state.selectedQues-1})}>Back</button>
-        <button onClick={()=>this.setState({selectedQues:this.state.selectedQues+1})}>Next</button>
+        { this.state.selectedQues > 0 &&
+        <button onClick={() => this.setState({ selectedQues: this.state.selectedQues - 1 })}>Back</button>}
+        { this.state.selectedQues < this.state.question.length - 1 &&
+        <button onClick={() => this.setState({ selectedQues: this.state.selectedQues + 1 })}>Next</button>}
       </div>
     );
   }
