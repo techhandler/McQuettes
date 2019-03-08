d10d154abce5a63268c79536d88b6e68198b4e79
Sumil <sumilprincejain@gmail.com>
2019-03-08 22:38:42 +0530

    Clean Questions Rendering

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,11 +2,21 @@ import React, { Component } from "react";
 
 export default class Question extends Component {
   render() {
-    let { data, selectedQues } = this.props;
+    let { data, selectedQues, selectAns } = this.props;
     return (
-      <div style={{border:'1px solid black'}}>
-        <p>{data[selectedQues].question}</p>
-        <p>{JSON.stringify(this.props)}</p>
+      <div style={{ border: '1px solid black' }} key={selectedQues}>
+        <p>{data.question}</p>
+        {data.options.map((a, i) => <p><input
+          type="radio"
+          name="ans"
+          id={i}
+          value={a}
+          onClick={() => {
+            selectAns({ selectedQues, selectedAns: i })
+          }}
+          checked={data.selectedAns === i}
+        />{a}</p>)}
+        {/*<p>{JSON.stringify(this.props)}</p>*/}
       </div>
     )
   }
