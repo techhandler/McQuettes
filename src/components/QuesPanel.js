9f50c9e678f180450267256354f121017e894dfa
Sumil <sumilprincejain@gmail.com>
2019-03-09 02:05:15 +0530

    adding quesPanel

--- src/components/QuesPanel.js
+++ src/components/QuesPanel.js
@@ -1,3 +1,34 @@
-/**
- * Created by sumil on 9/3/19.
- */
+import React, { Component } from "react";
+
+export default class QuestionPanel extends Component {
+  render() {
+    let { data } = this.props;
+    console.log("data", data);
+    return (
+      <div>
+        {data &&
+          data.map((a, i) => (
+            <span
+              style={a.selectedAns >= 0 ? style.attempted : style.notAttempted}
+              onClick={() => this.props.handlePanelClick(i)}
+            >
+              {i + 1}
+            </span>
+          ))}
+      </div>
+    );
+  }
+}
+
+let style = {
+  attempted: {
+    background: "green",
+    color: "white",
+    cursor: "pointer",
+    padding: "0 .5em"
+  },
+  notAttempted: {
+    background: "white",
+    cursor: "pointer"
+  }
+};
