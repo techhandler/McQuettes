6437488546ce4d69404160ecad1fc4f4a1d85703
Sumil <sumilprincejain@gmail.com>
2019-03-08 22:39:54 +0530

    Handling Selected answers

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import Question from "./components/Question";
 class App extends Component {
   state = { question, selectedQues: 0 };
 
+  selectAns ({ selectedQues, selectedAns }) {
+    this.setState((state)=>{state.question[selectedQues].selectedAns = selectedAns;return state});
+  }
+
   render() {
-    console.log(this.state.selectedQues);
+    console.log("sss",this.state.question);
     return (
       <div className="App">
-        <Question data={this.state.question} selectedQues={this.state.selectedQues}/>
+        <Question data={this.state.question[this.state.selectedQues]} selectedQues={this.state.selectedQues} selectAns={(e)=>this.selectAns(e)}/>
         { this.state.selectedQues > 0 &&
         <button onClick={() => this.setState({ selectedQues: this.state.selectedQues - 1 })}>Back</button>}
+        <span>{this.state.selectedQues}</span>
         { this.state.selectedQues < this.state.question.length - 1 &&
         <button onClick={() => this.setState({ selectedQues: this.state.selectedQues + 1 })}>Next</button>}
       </div>
