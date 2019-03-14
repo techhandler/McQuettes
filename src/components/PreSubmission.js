0c0981585e35d39b5cfd335758bbd31132bb8e86
Sumil <sumilprincejain@gmail.com>
2019-03-14 12:57:28 +0530

    adding pre submission

---
+++ src/components/PreSubmission.js
@@ -0,0 +1,13 @@
+/**
+ * Created by sumil on 14/3/19.
+ */
+
+import React, { Component } from "react";
+
+class App extends Component {
+  render(){
+    return (
+      <div>Submitted</div>
+    )
+  }
+}
